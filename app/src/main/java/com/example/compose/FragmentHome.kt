c893aa1aaf3c5583cebff75bf244f9c937fbe3aa
Kya3393 <ystsead@ust.hk>
2023-11-29 20:31:38 +0800

    Click FAB to intent new Activity

--- app/src/main/java/com/example/compose/FragmentHome.kt
+++ app/src/main/java/com/example/compose/FragmentHome.kt
@@ -1,7 +1,22 @@
 package com.example.compose
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class FragmentHome: Fragment(R.layout.fragment_home) {
     lateinit var categoryViewModel: CategoryViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fabAdd = view.findViewById<FloatingActionButton>(R.id.add_fab)
+        fabAdd.setOnClickListener{
+            Intent(requireContext(), EditRecordActivity::class.java).also{
+                startActivity(it)
+            }
+        }
+    }
 }
