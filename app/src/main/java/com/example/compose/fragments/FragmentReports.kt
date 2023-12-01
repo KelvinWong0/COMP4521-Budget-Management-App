74edee57309815ad200c1ca78bb1c4c71f65232a
Kya3393 <ystsead@ust.hk>
2023-12-01 18:26:46 +0800

    Done charts switch

--- app/src/main/java/com/example/compose/fragments/FragmentReports.kt
+++ app/src/main/java/com/example/compose/fragments/FragmentReports.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,6 +20,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import co.yml.charts.common.components.Legends
@@ -50,10 +52,32 @@ class FragmentReports : Fragment(R.layout.fragment_report){
 //    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val sbtnExpense = view.findViewById<Button>(R.id.btn_report_cat_1)
+        val sbtnIncome = view.findViewById<Button>((R.id.btn_report_cat_2))
+        val sbtnBalance = view.findViewById<Button>(R.id.btn_report_cat_3)
+
         val composeView = view.findViewById<ComposeView>(R.id.composeView)
+        //Default show Expense chart
         composeView.setContent {
             //Text(text = "Compose created")
             TestDountChart(requireContext())
+        }
+        // Switch between Charts
+        sbtnExpense.setOnClickListener {
+            composeView.setContent {
+                TestDountChart(requireContext())
+            }
+        }
+        sbtnIncome.setOnClickListener {
+            composeView.setContent {
+                IncomeDountChart(requireContext())
+            }
+        }
+        sbtnBalance.setOnClickListener {
+            composeView.setContent {
+                IncomeDountChart(requireContext())
+            }
         }
         dataViewModel = ViewModelProvider(this).get(DataViewModel::class.java)
     }
@@ -66,8 +90,57 @@ private fun TestDountChart(context: Context){
     //val data = DataUtils.getDonutChartData()
     val donutChartData = PieChartData(
         slices = listOf(
-            PieChartData.Slice("CAT 1", 15f, Color(0xFF5F0A87)),
-            PieChartData.Slice("CAT 2", 30f, Color(0xFF20BF55)),
+            PieChartData.Slice("CAT 1", 25f, Color(0xFF5F0A87)),
+            PieChartData.Slice("CAT 2", 25f, Color(0xFF20BF55)),
+            PieChartData.Slice("CAT 3", 25f,  Color(0xFFEC9F05)),
+            PieChartData.Slice("CAT 4", 25f, Color(0xFFF53844))
+        ),
+        plotType = PlotType.Donut
+    )
+    // Sum of all the values
+    val sumOfValues = donutChartData.totalLength
+
+    // Calculate each proportion value
+    val proportions = donutChartData.slices.proportion(sumOfValues)
+    val pieChartConfig =
+        PieChartConfig(
+            labelVisible = true,
+            strokeWidth = 120f,
+            labelColor = Color.Black,
+            activeSliceAlpha = .9f,
+            isEllipsizeEnabled = true,
+            labelTypeface = Typeface.defaultFromStyle(Typeface.BOLD),
+            isAnimationEnable = true,
+            chartPadding = 25,
+            labelFontSize = 42.sp,
+        )
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(500.dp)
+    ) {
+        Legends(legendsConfig = DataUtils.getLegendsConfigFromPieChartData(pieChartData = donutChartData, 3))
+        DonutPieChart(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(400.dp),
+            donutChartData,
+            pieChartConfig
+        ) { slice ->
+            Toast.makeText(context, slice.label, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+@Composable
+private fun IncomeDountChart(context: Context){
+
+    val scope = rememberCoroutineScope()
+    //val data = DataUtils.getDonutChartData()
+    val donutChartData = PieChartData(
+        slices = listOf(
+            PieChartData.Slice("CAT 1", 30f, Color(0xFF5F0A87)),
+            PieChartData.Slice("CAT 2", 20f, Color(0xFF20BF55)),
             PieChartData.Slice("CAT 3", 40f,  Color(0xFFEC9F05)),
             PieChartData.Slice("CAT 4", 10f, Color(0xFFF53844))
         ),
