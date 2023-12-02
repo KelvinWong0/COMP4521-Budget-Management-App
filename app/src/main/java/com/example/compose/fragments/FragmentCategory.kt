961105c6168e0e9ad47ed3e593fbb423d69d0a5e
KelvinWong0 <tmwongaf@connect.ust.hk>
2023-12-03 01:38:05 +0800

    commented out bug

--- app/src/main/java/com/example/compose/fragments/FragmentCategory.kt
+++ app/src/main/java/com/example/compose/fragments/FragmentCategory.kt
@@ -16,6 +16,7 @@ import com.example.compose.data.models.Category
 import com.example.compose.data.models.Record
 import com.example.compose.fragments.list.GridAdapter
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import java.sql.Date
 
 
 class FragmentCategory : Fragment(R.layout.fragment_category) {
@@ -81,11 +82,11 @@ class FragmentCategory : Fragment(R.layout.fragment_category) {
                 .show()
         }
 
-        gvCategory.onItemClickListener = OnItemClickListener { parent, view, position, id -> //here you can use the position to determine what checkbox to check
-            //this assumes that you have an array of your checkboxes as well. called checkbox
-            val grid = view.findViewById<com.google.android.material.card.MaterialCardView>(R.id.gridCat)
-            grid.get(position).setChecked(!checkBox.isChecked())
-        };
+//        gvCategory.onItemClickListener = OnItemClickListener { parent, view, position, id -> //here you can use the position to determine what checkbox to check
+//            //this assumes that you have an array of your checkboxes as well. called checkbox
+//            val grid = view.findViewById<com.google.android.material.card.MaterialCardView>(R.id.gridCat)
+//            grid.get(position).setChecked(!checkBox.isChecked())
+//        };
     }
 
 
@@ -97,14 +98,14 @@ class FragmentCategory : Fragment(R.layout.fragment_category) {
     }
 
     private fun tempDatabaseInsert(){
-        val record1 = Record(0,  "GTA6"     , Category(0,"games", R.drawable.ic_cat_entertainment, false)   , "200", "30-11-2023")
-        val record2 = Record(0,  "underwear", Category(0,"clothings", R.drawable.ic_cat_shopping, false)    , "-20", "30-11-2023")
-        val record3 = Record(0,  "water"    , Category(0,"bill", R.drawable.ic_cat_bills, false)            , "-60", "30-11-2023")
-        val record4 = Record(0,  "milk"     , Category(0,"grocery", R.drawable.ic_cat_grocery, false)       , "150", "30-11-2023")
-        dataViewModel.addRecord(record1)
-        dataViewModel.addRecord(record2)
-        dataViewModel.addRecord(record3)
-        dataViewModel.addRecord(record4)
+//        val record1 = Record(0,  "GTA6"     , Category(0,"games", R.drawable.ic_cat_entertainment, false)   , "200", Date(30-11-2023))
+//        val record2 = Record(0,  "underwear", Category(0,"clothings", R.drawable.ic_cat_shopping, false)    , "-20", "30-11-2023")
+//        val record3 = Record(0,  "water"    , Category(0,"bill", R.drawable.ic_cat_bills, false)            , "-60", "30-11-2023")
+//        val record4 = Record(0,  "milk"     , Category(0,"grocery", R.drawable.ic_cat_grocery, false)       , "150", "30-11-2023")
+//        dataViewModel.addRecord(record1)
+//        dataViewModel.addRecord(record2)
+//        dataViewModel.addRecord(record3)
+//        dataViewModel.addRecord(record4)
         Toast.makeText(requireContext(),"records added successful!", Toast.LENGTH_LONG).show()
     }
 }
