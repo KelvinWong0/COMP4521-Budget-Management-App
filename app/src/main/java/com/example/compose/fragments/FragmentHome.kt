a4bf835a9a58b22c1ca77053bff39902e1f46608
Kya3393 <ystsead@ust.hk>
2023-12-03 17:09:51 +0800

    also put Record List in Main fragment

--- app/src/main/java/com/example/compose/fragments/FragmentHome.kt
+++ app/src/main/java/com/example/compose/fragments/FragmentHome.kt
@@ -3,10 +3,17 @@ package com.example.compose.fragments
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.compose.DataViewModel
 import com.example.compose.EditRecordActivity
 import com.example.compose.R
+import com.example.compose.fragments.list.ListAdapter
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class FragmentHome: Fragment(R.layout.fragment_home) {
@@ -21,5 +28,15 @@ class FragmentHome: Fragment(R.layout.fragment_home) {
                 startActivity(it)
             }
         }
+
+        val rvRecord = view.findViewById<RecyclerView>(R.id.recordList)
+        val adapter = ListAdapter()
+        rvRecord.adapter = adapter
+        rvRecord.layoutManager = LinearLayoutManager(requireContext())
+
+        dataViewModel = ViewModelProvider(this).get(DataViewModel::class.java)
+        dataViewModel.readAllRecord.observe(viewLifecycleOwner, Observer{records ->
+            adapter.setData(records)
+        })
     }
 }
