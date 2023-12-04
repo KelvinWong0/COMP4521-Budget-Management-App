d5d4c9be010a4c01f94335a77fd739181c3f0bca
KelvinWong0 <tmwongaf@connect.ust.hk>
2023-12-04 16:48:33 +0800

    night mode

--- app/src/main/java/com/example/compose/fragments/FragmentSettings.kt
+++ app/src/main/java/com/example/compose/fragments/FragmentSettings.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.compose.R
@@ -14,15 +15,15 @@ class FragmentSettings : Fragment(R.layout.fragment_settings){
         super.onViewCreated(view, savedInstanceState)
 
         val sbtnNoti = view.findViewById<Switch>(R.id.switchNoti)
+        sbtnNoti.isChecked = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
         sbtnNoti.setOnCheckedChangeListener { _, isChecked ->
             when{
                 isChecked -> {
-                    if(ContextCompat.checkSelfPermission( view.context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
 
-                    }
                 }
                 else -> {
-
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
                 }
             }
         }
